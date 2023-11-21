ad2eabbbcb1c5b081db8439dd0f78379b148e7cd
florent <duparquet.florent@orange.fr>
2023-11-21 16:55:20 +0100

    ajout du telechargement du fichier

--- src/main/java/fr/DecoCN/controller/FicheController.java
+++ src/main/java/fr/DecoCN/controller/FicheController.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -64,4 +70,22 @@ public class FicheController {
 		model.addAttribute("fiche" , ficheService.getFicheById(id));
 		return "home/detail";
 	}
+	
+	@GetMapping("/private/detail/{id}/download")
+    public ResponseEntity<Resource> downloadFiche(@PathVariable long id) {
+        Fiche fiche = ficheService.getFicheById(id);
+
+        if (fiche != null && fiche.getContenu() != null) {
+            ByteArrayResource resource = new ByteArrayResource(fiche.getContenu());
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + fiche.getNomDeFichier())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .contentLength(fiche.getContenu().length)
+                    .body(resource);
+        } else {
+            // Gérer le cas où la fiche ou le contenu n'est pas trouvé
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/fr/DecoCN/controller/UtilisateurController.java
+++ src/main/java/fr/DecoCN/controller/UtilisateurController.java
@@ -1,6 +1,7 @@
 package fr.DecoCN.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.DecoCN.bll.UtilisateurService;
 import fr.DecoCN.bo.Utilisateur;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 
@@ -35,5 +38,16 @@ public class UtilisateurController {
 	public String creer(@ModelAttribute Utilisateur utilisateur) {
 		utilisateurService.CreerUtilisateur(utilisateur);
 		return "redirect:/private/gerer";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpServletResponse response) {
+		Cookie cookie = new Cookie("JSESSIONID", "nonConnecter");
+        cookie.setPath("/"); 
+        response.addCookie(cookie);
+
+        // Efface le contexte de sécurité
+        SecurityContextHolder.clearContext();
+		return "redirect:/";
 	}
 }
